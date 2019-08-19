0ab7b0250003fc84ac0b5fe85909ce01a215c20a
Kevin Wang <kevinwang@Kevins-MacBook-Pro.local>
2019-08-19 15:10:14 -0700

    bind challenge ez

--- udemyjs.js
+++ udemyjs.js
@@ -96,6 +96,21 @@ function Question (question, answers, correct) {
     this.correct = correct;
 };
 
+Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+        console.log(i + ': ' + this.answers[i])
+    }
+}
+
+Question.prototype.checkAnswer = function (answer) {
+    if(answer === this.correct) {
+        console.log('Correct Answer!')
+    } else {
+        console.log('Wrong Answer. Try again!')
+    }
+}
+
 var q1 = new Question ("Is Javascript the coolest programming language in the world?",
  ['yes', 'no'],
   0);
@@ -113,6 +128,10 @@ var q3 = new Question ("What does best describe coding?",
 var questions = [q1, q2, q3];
 
 
-var n = Math.floor(Math.random()) * questions.length;
+var n = Math.floor(Math.random() * questions.length);
 
-question[n]
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer.'));
+
+questions[n].checkAnswer(answer);
